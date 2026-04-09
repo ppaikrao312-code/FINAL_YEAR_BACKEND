ade45305af05d6821b5807d9cbc169f9e7deaaf9
ppaikrao312-code <ppaikrao312@gmail.com>
2026-04-09 22:05:34 +0530

    Update CorsConfig.java

--- src/main/java/com/digitalfir/security/CorsConfig.java
+++ src/main/java/com/digitalfir/security/CorsConfig.java
@@ -19,10 +19,7 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of(
             "http://127.0.0.1:5500",
             "http://localhost:5500",
-            "http://34.235.155.152",
-            "http://34.235.155.152:8080",
-        
-            	    "http://digitalfirfrontend2026.s3-website-us-east-1.amazonaws.com"
+            "https://finalyearbackend-production-09c1.up.railway.app"
             	
         ));
 
