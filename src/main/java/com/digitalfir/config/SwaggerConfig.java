0a1183e4ce43d240d6ff2be8f50d81951b901dda
ppaikrao312-code <ppaikrao312@gmail.com>
2026-04-09 22:18:01 +0530

    Update SwaggerConfig.java

--- src/main/java/com/digitalfir/config/SwaggerConfig.java
+++ src/main/java/com/digitalfir/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +14,12 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
 
+        // ✅ Force HTTPS Railway URL
+        Server server = new Server();
+        server.setUrl("https://finalyearbackend-production-09c1.up.railway.app");
+
         return new OpenAPI()
+                .addServersItem(server) // 🔥 Important
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(
                         new Components().addSecuritySchemes(
@@ -27,4 +33,3 @@ public class SwaggerConfig {
                 );
     }
 }
-
