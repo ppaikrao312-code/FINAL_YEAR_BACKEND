6ac5ae234cdf2e2d17204e66533bd288ee52b37f
ppaikrao312-code <ppaikrao312@gmail.com>
2026-04-10 05:58:09 +0530

    Update WebConfig.java

--- src/main/java/com/digitalfir/config/WebConfig.java
+++ src/main/java/com/digitalfir/config/WebConfig.java
@@ -8,19 +8,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    // ✅ CORS CONFIG
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                .allowedOrigins(
+                        "http://127.0.0.1:5500",
+                        "http://localhost:5500",
+                        "https://finalyearbackend-production-09c1.up.railway.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 
-    // Static resource setup
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/home/ubuntu/digital-fir-backend/uploads/");
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
     }
 }
